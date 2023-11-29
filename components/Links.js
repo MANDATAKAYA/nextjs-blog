cc675f86f6093706ed82af32663d7ef27ba0d5eb
MANDATAKAYA <gangyewantian@gmail.com>
2023-11-29 21:43:48 +0900

    ♻️ Linksのリファレンス

--- components/Links.js
+++ components/Links.js
@@ -1,36 +1,22 @@
-import Head from 'next/head';
 import styles from './Links.module.css';
 
+const ITEMS = [
+  { href: "https://nextjs.org/docs", title: "Documentation \u2192", description: "Find in-depth information about Next.js features and API." },
+  { href: "https://nextjs.org/learn", title: "Learn \u2192", description: "Learn about Next.js in an interactive course with quizzes!" },
+  { href: "https://github.com/vercel/next.js/tree/canary/examples", title: "Examples \u2192", description: "Discover and deploy boilerplate example Next.js projects." },
+  { href: "https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app", title: "Deploy \u2192", description: "Instantly deploy your Next.js site to a public URL with Vercel." },
+]
 export function Links() {
   return (
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+    <div className={styles.grid}>
+      {ITEMS.map(item => {
+        return (
+          <a key={item.href} href={item.href} className={styles.card}>
+            <h3 className={styles.title}>{item.title}</h3>
+            <p className={styles.description}>{item.description}</p>
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        );
+      })}
+    </div>
   )
 }
